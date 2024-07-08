06facb3daf981f9b1530eaedf5978de8e8711203
haykam821 <24855774+haykam821@users.noreply.github.com>
2024-07-08 18:46:17 -0400

    Update minigame to Minecraft 1.20.4

--- src/main/java/io/github/haykam821/volleyball/entity/VolleyballEntityTypes.java
+++ src/main/java/io/github/haykam821/volleyball/entity/VolleyballEntityTypes.java
@@ -3,8 +3,6 @@ package io.github.haykam821.volleyball.entity;
 import eu.pb4.polymer.core.api.entity.PolymerEntityUtils;
 import io.github.haykam821.volleyball.Volleyball;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnGroup;
@@ -14,8 +12,8 @@ import net.minecraft.util.Identifier;
 
 public final class VolleyballEntityTypes {
 	private static final Identifier BALL_ID = new Identifier(Volleyball.MOD_ID, "ball");
-	public static final EntityType<BallEntity> BALL = FabricEntityTypeBuilder.<BallEntity>create(SpawnGroup.MISC, BallEntity::new)
-		.dimensions(EntityDimensions.changing(8 / 16f, 8 / 16f))
+	public static final EntityType<BallEntity> BALL = EntityType.Builder.<BallEntity>create(BallEntity::new, SpawnGroup.MISC)
+		.setDimensions(8 / 16f, 8 / 16f)
 		.build();
 
 	private VolleyballEntityTypes() {
